6e923e0946e7242aa99456c20f0d2c12b610860a
Mogankumar Narsozhan <155053769+Mogankumar@users.noreply.github.com>
2025-11-08 18:49:37 -0500

    Add new functions for UHI and traffic delay calculations

--- hackathon@Beagle/models.py
+++ hackathon@Beagle/models.py
@@ -17,3 +17,13 @@ def pm25_delta(df, c0=7.0, c1=0.03, c2=3.0):
     delay = traffic_delay_pct(df)/100
     pm = c0 + c1*delay + c2*(1 - df["green_cov"])
     return (pm - pm.mean())
+
+def uhi_raw(df, a1=6.0, a2=4.0, a3=1.0, water_adj=0.0):
+    return a1*df["impervious"] - a2*df["green_cov"] - a3*water_adj
+
+def traffic_delay_raw(df, b1=0.6, b2=0.3, b3=0.2):
+    return b1*df["road_den"] + b2*df["int_den"] + b3*df["building_cov"]
+
+def pm25_raw(df, c0=7.0, c1=0.03, c2=3.0):
+    delay = traffic_delay_raw(df)
+    return c0 + c1*delay + c2*(1 - df["green_cov"])
